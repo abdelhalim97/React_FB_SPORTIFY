62a50c6a7ef47f61b483990cda3ca052f6a03814
abdelhalim97 <abdelhalimbenoun@gmail.com>
2022-02-17 19:21:40 +0100

    reset pass

--- src/components/containers/form.jsx
+++ src/components/containers/form.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
-import {ReButton,ReInput} from "./units"
-import { Button, Container, Typography } from '@material-ui/core'
-import { createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import {ReInput} from "./units"
+import { Button, Container, TextField, Typography } from '@material-ui/core'
+import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { auth } from '../../auth/firebase'
 export const Form = () => {
+  const [forgetPass, setForgetPass] = useState('')
+  const [forgetPassStat, setForgetPassStat] = useState(0)
   const [form, setForm] = useState('login')
   const [formData, setFormData] = useState({})
   const [formDataLogIn, setFormDataLogIn] = useState({})
+  const logIn=async()=>{
+    try {
+      const {email,password}=formDataLogIn
+      await signInWithEmailAndPassword(auth,email,password)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const handleSetFormtoForgotPasword=()=>setForm('forgotPassword')
   const dataButtons =[
     {
       id:0,
@@ -19,6 +30,22 @@ export const Form = () => {
       title:"SignUp",
       styles:"text-third bg-sec rounded-2xl",
       param:"signUp",
+    },
+  ]
+  const dataButtons2 =[
+    {
+      id:0,
+      title:"Login",
+      fnc:logIn,
+      styles:'text-third bg-base my-3 rounded-2xl',
+      variant:'contained'
+    },
+    {
+      id:1,
+      title:"Forgot Password?",
+      fnc:handleSetFormtoForgotPasword,
+      styles:'my-3 text-base text-xs',
+      variant:'text'
     },
   ]
   const dataLogin =[
@@ -74,12 +101,12 @@ export const Form = () => {
       console.log(error)
     }
   }
-  const logIn=async()=>{
+  const resetPassword=async()=>{
     try {
-      const {email,password}=formDataLogIn
-      await signInWithEmailAndPassword(auth,email,password)
+    await sendPasswordResetEmail(auth,forgetPass)
+    setForgetPassStat(1)
     } catch (error) {
-      console.log(error)
+    setForgetPassStat(2)
     }
   }
   return (
@@ -87,9 +114,10 @@ export const Form = () => {
         <div className='border-2 border-sec w-1/2 absolute top-1/2 rounded-3xl bg-third shadow-lg'>
           <Container maxWidth='xs'>
               <div className='flex justify-around my-3'>
-                {dataButtons.map(data=><ReButton key={data.id} variant='contained' title={data.title} fnc={()=>{setForm(data.param)}}
-                styles={`${form==='login'&&data.id===0?'text-third bg-sec rounded-2xl':form==='signUp'&&data.id===1?'text-third bg-sec rounded-2xl':
-                form==='login'&&data.id===1?'text-sec bg-third rounded-2xl':'text-sec bg-third rounded-2xl'}`}></ReButton>)}
+                {dataButtons.map(data=>
+                <Button key={data.id} variant='contained' onClick={()=>{setForm(data.param);setForgetPassStat(0)}}
+                className={`${form==='login'&&data.id===0?'text-third bg-sec rounded-2xl':form==='signUp'&&data.id===1?'text-third bg-sec rounded-2xl':
+                form==='login'&&data.id===1?'text-sec bg-third rounded-2xl':'text-sec bg-third rounded-2xl'}`}>{data.title}</Button>)}
               </div>
             {form === 'login'&&<>
               <div className='mx-auto w-5/6 my-2'>
@@ -97,9 +125,11 @@ export const Form = () => {
                 <ReInput key={data.id} value={formDataLogIn[data.id]} onChange={(e)=>{handleChangeLogIn(e.target.value,data.id)}} label={data.label} types={data.types} styles="w-full my-1"/>
                 )}
               </div>
-              <div className='flex justify-center'>
-                <Button variant='contained' onClick={()=>{logIn()}} className='text-third bg-base my-3 rounded-2xl'>Login</Button>
-              </div>
+              {dataButtons2.map(data=>
+                <div key={data.id} className='flex justify-center'>
+                  <Button variant={data.variant} onClick={()=>{data.fnc()}} className={data.styles}>{data.title}</Button>
+                </div>
+                )}
               <Typography variant='subtitle2' className='my-3 text-sec text-center text-sm'>Don't have an account?
               <Button variant='text' className='my-3 text-base text-xs' onClick={()=>{setForm('signUp')}}>Sign Up</Button>
               </Typography>
@@ -116,8 +146,23 @@ export const Form = () => {
                 </div>
               </form>
               <Typography variant='subtitle2' className='my-3 text-sec text-center text-sm'>Already have an account?
-              <ReButton title='LogIn' variant='text' styles='my-3 text-base text-xs' fnc={()=>{setForm('login')}}/>
+              <Button variant='text' className='my-3 text-base text-xs' onClick={()=>{setForm('login')}}>LogIn</Button>
               </Typography>
+            </>}
+            {form === 'forgotPassword'&&<>
+              <form>
+                <div className='mx-auto w-5/6 my-2'>
+                  <TextField variant="standard" value={forgetPass} onChange={(e)=>{setForgetPass(e.target.value)}} label="Forgot Password" className="w-full my-1 "/>
+                </div>
+              {forgetPassStat===0?<></>:forgetPassStat===1?<Typography className='text-center my-2' variant='body2' >Email has been sent</Typography>:<Typography className='text-center my-2' variant='body2'>something went wrong</Typography>}
+
+                <div className='flex justify-center'>
+                  <Button variant='contained' className='text-third bg-base my-3 rounded-2xl' onClick={()=>{resetPassword()}}>Reset Password</Button>
+                </div>
+              </form>
+              <div className='flex justify-center'>
+                <Button variant='text' className='my-3 text-base text-xs' onClick={()=>{setForm('login');setForgetPassStat(0)}}>Back to LogIn</Button>
+              </div>
             </>}
           </Container>
           </div>
