98613ae9b9641f21eac9df07e7eac2e5024c8094
unknown <abdelhalimbenoun@gmail.com>
2022-06-28 12:31:34 +0100

    debug home

--- src/components/containers/cards.jsx
+++ src/components/containers/cards.jsx
@@ -1,18 +1,33 @@
 import { Grid } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useFetchStadiums from '../../custom-hooks/useFetchStadiums'
-import useFetchReservationsUser from '../../custom-hooks/useFetchReservationsUser'
 import { Card } from './units'
 import iconStadium from '../../assets/images/stadium.png'
 import iconReservation from '../../assets/images/reservation.png'
 import iconMoney from '../../assets/images/money.png'
+import { db } from '../../auth/firebase'
+import { onValue, ref } from 'firebase/database'
 
 export const Cards = () => {
+    const [rentData, setRentData] = useState([]);
     const stadiumsData = useFetchStadiums()
     const stadiumsUserArrya=stadiumsData.map(d=>d.uid)
-    const reservationUserData = useFetchReservationsUser(stadiumsUserArrya)
-    // console.log(reservationUserData)
-    const reservationCost =reservationUserData.reduce((accumulator,currentElement,index)=>parseFloat(accumulator)+((
+    useEffect(() => {
+        if(stadiumsUserArrya){
+          onValue(ref(db,'reservation'),(snapshot)=>{
+            setRentData([])
+            const dataLocalRent = snapshot.val();
+            if(dataLocalRent!==null){
+              Object.values(dataLocalRent).map((d)=>{
+                if(d.stadiumUid.includes(stadiumsUserArrya))
+                setRentData((oldArray)=>[...oldArray,d]);
+                return 0
+              })
+            }
+          })  
+        }
+      }, [stadiumsData])
+    const reservationCost = rentData.reduce((accumulator,currentElement)=>parseFloat(accumulator)+((
         (parseInt(currentElement.toHours)*60+parseInt(currentElement.toMinutes))-(parseInt(currentElement.fromHours)*60+parseInt(currentElement.fromMinutes))
     )/60)*parseInt(currentElement.cost),'0')
     const reservationRound= parseFloat(reservationCost).toFixed(2)
@@ -25,7 +40,7 @@ export const Cards = () => {
     },
     {
         id:1,
-        dataLength:reservationUserData.length,
+        dataLength:rentData.length,
         desc:'Reservations',
         icon:iconReservation
     },
--- src/custom-hooks/useFetchReservationsUser.js
+++ src/custom-hooks/useFetchReservationsUser.js
@@ -5,12 +5,13 @@ import { db } from "../auth/firebase";
 const useFetchReservationsUser = (terrain)=>{
     const [rentData, setRentData] = useState([]);
     useEffect(() => {
-        if(terrain.length>0){
+        if(terrain){
           onValue(ref(db,'reservation'),(snapshot)=>{
             setRentData([])
             const dataLocalRent = snapshot.val();
             if(dataLocalRent!==null){
               Object.values(dataLocalRent).map((d)=>{
+                console.log(d.stadiumUid.includes(terrain))
                 if(d.stadiumUid.includes(terrain))
                 setRentData((oldArray)=>[...oldArray,d]);
                 return 0
