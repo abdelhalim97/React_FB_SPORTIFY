60c82d6914454430691520c1bcf0663256cac9a2
Abdelhalim ben oun <abdelhalimbenoun@gmail.com>
2023-09-17 16:55:51 +0100

    fix fetching cards data

--- src/components/containers/cards.jsx
+++ src/components/containers/cards.jsx
@@ -1,62 +1,78 @@
-import { Grid } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import useFetchStadiums from '../../custom-hooks/useFetchStadiums'
-import { Card } from './units'
-import iconStadium from '../../assets/images/stadium.png'
-import iconReservation from '../../assets/images/reservation.png'
-import iconMoney from '../../assets/images/money.png'
-import { db } from '../../auth/firebase'
-import { onValue, ref } from 'firebase/database'
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import useFetchStadiums from "../../custom-hooks/useFetchStadiums";
+import { Card } from "./units";
+import iconStadium from "../../assets/images/stadium.png";
+import iconReservation from "../../assets/images/reservation.png";
+import iconMoney from "../../assets/images/money.png";
+import { db } from "../../auth/firebase";
+import { onValue, ref } from "firebase/database";
 
 export const Cards = () => {
-    const [rentData, setRentData] = useState([]);
-    const stadiumsData = useFetchStadiums()
-    const stadiumsUserArrya=stadiumsData.map(d=>d.uid)
-    useEffect(() => {
-        if(stadiumsUserArrya){
-          onValue(ref(db,'reservation'),(snapshot)=>{
-            setRentData([])
-            const dataLocalRent = snapshot.val();
-            if(dataLocalRent!==null){
-              Object.values(dataLocalRent).map((d)=>{
-                if(d.stadiumUid.includes(stadiumsUserArrya))
-                setRentData((oldArray)=>[...oldArray,d]);
-                return 0
-              })
-            }
-          })  
+  const [rentData, setRentData] = useState([]);
+  const stadiumsData = useFetchStadiums();
+  const stadiumsUserArrya = stadiumsData.map((d) => d.uid);
+  useEffect(() => {
+    if (stadiumsUserArrya) {
+      onValue(ref(db, "reservation"), (snapshot) => {
+        setRentData([]);
+        const dataLocalRent = snapshot.val();
+        if (dataLocalRent !== null) {
+          Object.values(dataLocalRent).map((d) => {
+            if (d.stadiumUid.includes(stadiumsUserArrya))
+              setRentData((oldArray) => [...oldArray, d]);
+            return 0;
+          });
         }
-      }, [stadiumsData])
-    const reservationCost = rentData.reduce((accumulator,currentElement)=>parseFloat(accumulator)+((
-        (parseInt(currentElement.toHours)*60+parseInt(currentElement.toMinutes))-(parseInt(currentElement.fromHours)*60+parseInt(currentElement.fromMinutes))
-    )/60)*parseInt(currentElement.cost),'0')
-    const reservationRound= parseFloat(reservationCost).toFixed(2)
-    const cardsData = [
-        {
-        id:0,
-        dataLength:stadiumsData.length,
-        desc:'Stadiums',
-        icon:iconStadium
+      });
+    }
+  }, []);
+
+  const reservationCost = rentData.reduce(
+    (accumulator, currentElement) =>
+      parseFloat(accumulator) +
+      ((parseInt(currentElement.toHours) * 60 +
+        parseInt(currentElement.toMinutes) -
+        (parseInt(currentElement.fromHours) * 60 +
+          parseInt(currentElement.fromMinutes))) /
+        60) *
+        parseInt(currentElement.cost),
+    "0"
+  );
+  const reservationRound = parseFloat(reservationCost).toFixed(2);
+  const cardsData = [
+    {
+      id: 0,
+      dataLength: stadiumsData.length,
+      desc: "Stadiums",
+      icon: iconStadium,
     },
     {
-        id:1,
-        dataLength:rentData.length,
-        desc:'Reservations',
-        icon:iconReservation
+      id: 1,
+      dataLength: rentData.length,
+      desc: "Reservations",
+      icon: iconReservation,
     },
     {
-        id:2,
-        dataLength:reservationRound,
-        desc:'Earned',
-        icon:iconMoney
+      id: 2,
+      dataLength: reservationRound,
+      desc: "Earned",
+      icon: iconMoney,
     },
-]
+  ];
+  console.log(cardsData);
   return (
     <>
-        {cardsData.map(cardData=>
+      {cardsData.map((cardData) => (
         <Grid key={cardData.id} item xs={12} sm={4}>
-            <Card desc={cardData.desc} nb={cardData.dataLength} icon={cardData.icon} id={cardData.id} />
-        </Grid>)}
+          <Card
+            desc={cardData.desc}
+            nb={cardData.dataLength}
+            icon={cardData.icon}
+            id={cardData.id}
+          />
+        </Grid>
+      ))}
     </>
-  )
-}
+  );
+};
--- src/components/reservation.jsx
+++ src/components/reservation.jsx
@@ -1,24 +1,38 @@
-import { CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { Border,Pagination,TypographyIcon } from './containers/units'
-import useFetchStadiums from '../custom-hooks/useFetchStadiums';
-import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
-import useFetchReservations from '../custom-hooks/useFetchReservations';
-import { TablePaginationActions } from './containers';
+import {
+  CircularProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Border, Pagination, TypographyIcon } from "./containers/units";
+import useFetchStadiums from "../custom-hooks/useFetchStadiums";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import useFetchReservations from "../custom-hooks/useFetchReservations";
+import { TablePaginationActions } from "./containers";
 
 export const Reservation = () => {
-  const [loading, setLoading] = useState(true)
-  const data = useFetchStadiums()
+  const [loading, setLoading] = useState(true);
+  const data = useFetchStadiums();
   const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [page, setPage] = useState(0)
-  const [rentUid, setRentUid] = useState('')
-  const [pageNumber, setPageNumber] = useState(0)
-  const dataPerPage=1
-  const pagesVisited=dataPerPage*pageNumber
+  const [page, setPage] = useState(0);
+  const [rentUid, setRentUid] = useState("");
+  const [pageNumber, setPageNumber] = useState(0);
+  const dataPerPage = 1;
+  const pagesVisited = dataPerPage * pageNumber;
   useEffect(() => {
-    data?.slice(pagesVisited,pagesVisited+dataPerPage).map((d)=>{setRentUid(d.uid); return setLoading(false)})
-  }, [data,pageNumber])
-  const dataReservations = useFetchReservations(rentUid)
+    data?.slice(pagesVisited, pagesVisited + dataPerPage).map((d) => {
+      setRentUid(d.uid);
+      return setLoading(false);
+    });
+  }, [data, pageNumber]);
+  const dataReservations = useFetchReservations(rentUid);
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
@@ -27,51 +41,123 @@ export const Reservation = () => {
     setPage(newPage);
   };
   return (
-      <Border >
-      <Typography align='center' variant='h5' className='font-bold text-base text-4xl tex-center w-full py-1'>{data&&data.map(d=>d.uid===rentUid&&d.name)}</Typography>
-        {!loading ? data?.slice(pagesVisited,pagesVisited+dataPerPage).map((d,i)=>
-        <Paper className='w-full mb-3 ' key={i}>
-          <TableContainer component={Paper} className='mb-3'>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Cost</TableCell>
-                  <TableCell align="left">Date</TableCell>
-                  <TableCell align="left">Reservation from</TableCell>
-                  <TableCell align="left">Reservation to</TableCell>
-                  <TableCell align="left">Duration</TableCell>
-                  <TableCell align="left">reserver Email</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {(rowsPerPage > 0? dataReservations.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage): dataReservations).map((row) => ( 
-                  <TableRow key={row.uid} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                    <TableCell component="th" scope="row">{row.cost}</TableCell>
-                    <TableCell align="left">{row.day}/{row.month}/{row.year}</TableCell>
-                    <TableCell align="left">{row.fromHours}:{row.fromMinutes}</TableCell>
-                    <TableCell align="left">{row.toHours}:{row.toMinutes}</TableCell>
-                    <TableCell align="left">
-                      {Math.floor((parseInt(row.toHours*60)+parseInt(row.toMinutes)-(parseInt(row.fromHours*60)+parseInt(row.fromMinutes)))/60)}:
-                      {Math.floor((parseInt(row.toHours*60)+parseInt(row.toMinutes)-(parseInt(row.fromHours*60)+parseInt(row.fromMinutes)))%60)}
-                    </TableCell>
-                    <TableCell align="left">{row.reserverEmail}</TableCell>
+    <Border>
+      <Typography
+        align="center"
+        variant="h5"
+        className="font-bold text-base text-4xl tex-center w-full py-1"
+      >
+        {data && data.map((d) => d.uid === rentUid && d.name + " Stadium")}
+      </Typography>
+      {!loading ? (
+        data?.slice(pagesVisited, pagesVisited + dataPerPage).map((d, i) => (
+          <Paper className="w-full mb-3 " key={i}>
+            <TableContainer component={Paper} className="mb-3">
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Cost</TableCell>
+                    <TableCell align="left">Date</TableCell>
+                    <TableCell align="left">Reservation from</TableCell>
+                    <TableCell align="left">Reservation to</TableCell>
+                    <TableCell align="left">Duration</TableCell>
+                    <TableCell align="left">reserver Email</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <div className='flex justify-center'>
-          <TablePagination rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]} colSpan={3} count={dataReservations.length} rowsPerPage={rowsPerPage}  
-          page={page} SelectProps={{inputProps: {'aria-label': 'rows per page',},native: true,}} onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage} ActionsComponent={TablePaginationActions}/>
-          </div>
-        </Paper>):<div className='w-full flex justify-center'><CircularProgress/></div>}
-              {(dataReservations.length===0 && data.length>0)&&
-              <TypographyIcon variant='body1' styles='text-red-600 text-center mb-3 w-full' icon={faTriangleExclamation} text='There s no reservation yet'/>}
-        <div className='mx-auto'>
-          <Pagination data={data} setPageNumber={setPageNumber} dataPerPage={dataPerPage} setRentUid={setRentUid} />
+                </TableHead>
+                <TableBody>
+                  {(rowsPerPage > 0
+                    ? dataReservations.slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                    : dataReservations
+                  ).map((row) => (
+                    <TableRow
+                      key={row.uid}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.cost}
+                      </TableCell>
+                      <TableCell align="left">
+                        {row.day}/{row.month}/{row.year}
+                      </TableCell>
+                      <TableCell align="left">
+                        {row.fromHours}:{row.fromMinutes}
+                      </TableCell>
+                      <TableCell align="left">
+                        {row.toHours}:{row.toMinutes}
+                      </TableCell>
+                      <TableCell align="left">
+                        {Math.floor(
+                          (parseInt(row.toHours * 60) +
+                            parseInt(row.toMinutes) -
+                            (parseInt(row.fromHours * 60) +
+                              parseInt(row.fromMinutes))) /
+                            60
+                        )}
+                        :
+                        {Math.floor(
+                          (parseInt(row.toHours * 60) +
+                            parseInt(row.toMinutes) -
+                            (parseInt(row.fromHours * 60) +
+                              parseInt(row.fromMinutes))) %
+                            60
+                        )}
+                      </TableCell>
+                      <TableCell align="left">{row.reserverEmail}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <div className="flex justify-center">
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={dataReservations.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </div>
+          </Paper>
+        ))
+      ) : (
+        <div className="w-full flex justify-center">
+          <CircularProgress />
         </div>
-        {data.length===0 && <TypographyIcon variant='body1' styles='text-red-600 text-center mb-3 mx-auto' icon={faTriangleExclamation} text='you dont have any Stadiums yet'/>}
-      </Border>
-  )
-}
+      )}
+      {dataReservations.length === 0 && data.length > 0 && (
+        <TypographyIcon
+          variant="body1"
+          styles="text-red-600 text-center mb-3 w-full"
+          icon={faTriangleExclamation}
+          text="There s no reservation yet"
+        />
+      )}
+      <div className="mx-auto">
+        <Pagination
+          data={data}
+          setPageNumber={setPageNumber}
+          dataPerPage={dataPerPage}
+          setRentUid={setRentUid}
+        />
+      </div>
+      {data.length === 0 && (
+        <TypographyIcon
+          variant="body1"
+          styles="text-red-600 text-center mb-3 mx-auto"
+          icon={faTriangleExclamation}
+          text="you dont have any Stadiums yet"
+        />
+      )}
+    </Border>
+  );
+};
